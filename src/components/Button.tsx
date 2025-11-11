50d86498e22897d272637f8d13bdac7aef759aa3
dokkanoT <thmu1155@gmail.com>
2025-11-11 18:30:46 +0900

    Update link URL in Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ export default function Button() {
     return (
         <>
             <h2>ボタンをクリックしてあなたの恋愛運をチェック！</h2>
-             <Link href="https://hurihuriomikuji-6vm1qpaea-dokkanots-projects.vercel.app/result"> 
+             <Link href="https://hurihuriomikuji-5ycprfs0y-dokkanots-projects.vercel.app/result"> 
             <button className="button">プレイ</button>
             </Link>
         </>
